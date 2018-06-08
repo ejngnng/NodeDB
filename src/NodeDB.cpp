4e97284ae4163e38a4211b18ca4961a90c3096f4
ninja_57 <ninja_57@baicells.com>
2018-06-08 15:30:43 +0800

    create project

---
+++ src/NodeDB.cpp
@@ -0,0 +1,9 @@
+/*
+ * Description: node database source file
+ *
+ * Author: ninja
+ *
+ * Date: 2018-06-08
+ */
+ #include "NodeDB.h"
+ 
---
+++ src/NodeDB.h
@@ -0,0 +1,14 @@
+/*
+ * Description: node database header file
+ *
+ * Author: ninja
+ *
+ * Date: 2018-06-08
+ */
+#ifndef NodeDB_h
+#define NodeDB_h
+
+#include <Arduino.h>
+
+
+#endif
